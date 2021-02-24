44b8acbfc560da8fd96c1ba9667c6424e3106db9
Joseph Bokarov <>
2021-02-24 11:09:30 +0100

    re-enable purge css for optimisation

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "postcss-fontpath": { checkFiles: true, ie8Fix: true },
     tailwindcss: "tailwind.config.js",
-    //"@fullhuman/postcss-purgecss": process.env.NODE_ENV === "production",
+    "@fullhuman/postcss-purgecss": process.env.NODE_ENV === "production",
     autoprefixer: {},
   },
 };
